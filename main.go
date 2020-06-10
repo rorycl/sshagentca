9aea623c4353a267cbfad6a0734b702015324e37
rorycl <rorycl>
2020-06-10 12:44:14 +0100

    Main: remove authorized_keys option, allow env variables for passwords

--- main.go
+++ main.go
@@ -4,36 +4,39 @@ import (
 	"fmt"
 	flags "github.com/jessevdk/go-flags"
 	"github.com/rorycl/sshagentca/util"
+	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
 	"net"
 	"os"
 )
 
-const VERSION = "0.0.4-beta"
+const VERSION = "0.0.5-beta"
 const usage = `<options> <yamlfile>
 
-SSH Agent CA version %s
+SSHAgentCA version %s
 
-A proof-of-concept SSH server forwarded agent certificate authority
+An SSH server forwarded agent client certificate authority
 
     sshagentca -h
-    sshagentca -p <privatekey> -c <caprivatekey> -a <authorized_keys>
-               -i <ipaddress> -p <port> settings.yaml
+    sshagentca -t <privatekey> -c <caprivatekey> -i <ipaddress> -p <port>
+               <settings.yaml>
+
+The environmental variables SSHAGENTCA_PVT_KEY and SSHAGENTCA_CA_KEY may
+be used for the privatekey passwords. The server private key password is
+optional.
 
 Application Arguments:
-
 
  `
 
 // flag options
 type Options struct {
-	PrivateKey     string `short:"t" long:"privateKey" required:"true" description:"server ssh private key (password protected)"`
-	CAPrivateKey   string `short:"c" long:"caPrivateKey" required:"true" description:"certificate authority private key (password protected)"`
-	AuthorizedKeys string `short:"a" long:"authorizedKeys" required:"true" description:"authorized keys file with at least one entry"`
-	IPAddress      string `short:"i" long:"ipAddress" default:"0.0.0.0" description:"ipaddress"`
-	Port           string `short:"p" long:"port" default:"2222" description:"port"`
-	Args           struct {
-		YamlFile string `description:"settings yaml file"`
+	PrivateKey   string `short:"t" long:"privateKey" required:"true" description:"server ssh private key (optionally password protected)"`
+	CAPrivateKey string `short:"c" long:"caPrivateKey" description:"certificate authority private key file (password protected)"`
+	IPAddress    string `short:"i" long:"ipAddress" default:"0.0.0.0" description:"ipaddress"`
+	Port         string `short:"p" long:"port" default:"2222" description:"port"`
+	Args         struct {
+		Settings string `description:"settings yaml file"`
 	} `positional-args:"yes" required:"yes"`
 }
 
@@ -44,40 +47,66 @@ func hardexit(msg string) {
 
 func main() {
 
+	var err error
 	var options Options
 	var parser = flags.NewParser(&options, flags.Default)
 	parser.Usage = fmt.Sprintf(usage, VERSION)
 
-	if _, err := parser.Parse(); err != nil {
+	if _, err = parser.Parse(); err != nil {
 		os.Exit(1)
 	}
 
 	fmt.Println("SSH Agent CA")
 
-	// load server private key
-	fmt.Printf("\nServer private key password: ")
-	pvtPW, err := terminal.ReadPassword(0)
-	if err != nil {
-		hardexit(fmt.Sprintf("Could not read password: %s", err))
-	}
-	privateKey, err := util.LoadPrivateKeyWithPassword(options.PrivateKey, pvtPW)
-	if err != nil {
-		hardexit(fmt.Sprintf("Private key could not be loaded, %s", err))
+	// load server private key, first trying with no password
+	var privateKey ssh.Signer
+	privateKey, err = util.LoadPrivateKey(options.PrivateKey)
+	if err != nil && err != util.ErrKeyPassphraseRequired {
+		hardexit(fmt.Sprintf("Unexpected error: %s", err))
+
+		// retry with password
+	} else if err == util.ErrKeyPassphraseRequired {
+		var pvtPW = []byte{}
+		pvtPWstr := os.Getenv("SSHAGENTCA_PVT_KEY")
+		if pvtPWstr != "" {
+			pvtPW = []byte(pvtPWstr)
+			_ = os.Unsetenv("SSHAGENTCA_PVT_KEY")
+		} else {
+			fmt.Printf("\nServer private key password: ")
+			pvtPW, err = terminal.ReadPassword(0)
+			if err != nil {
+				hardexit(fmt.Sprintf("Could not read password: %s", err))
+			}
+		}
+		privateKey, err = util.LoadPrivateKeyWithPassword(options.PrivateKey, pvtPW)
+		if err != nil {
+			hardexit(fmt.Sprintf("Private key could not be loaded, %s", err))
+		}
+		pvtPW = nil
 	}
 
 	// load certificate authority private key
-	fmt.Printf("\nCertificate Authority private key password: ")
-	caPW, err := terminal.ReadPassword(0)
-	if err != nil {
-		hardexit(fmt.Sprintf("Could not read password: %s", err))
+	var caKey ssh.Signer
+	var caPW = []byte{}
+	caPWstr := os.Getenv("SSHAGENTCA_CA_KEY")
+	if caPWstr != "" {
+		caPW = []byte(caPWstr)
+		_ = os.Unsetenv("SSHAGENTCA_CA_KEY")
+	} else {
+		fmt.Printf("\nCertificate Authority private key password: ")
+		caPW, err = terminal.ReadPassword(0)
+		if err != nil {
+			hardexit(fmt.Sprintf("Could not read password: %s", err))
+		}
 	}
-	caKey, err := util.LoadPrivateKeyWithPassword(options.CAPrivateKey, caPW)
+	caKey, err = util.LoadPrivateKeyWithPassword(options.CAPrivateKey, caPW)
 	if err != nil {
 		hardexit(fmt.Sprintf("CA Private key could not be loaded, %s", err))
 	}
+	caPW = nil
 
-	// load settings and authorized keys
-	settings, err := util.SettingsLoad(options.Args.YamlFile, options.AuthorizedKeys)
+	// load settings yaml file
+	settings, err := util.SettingsLoad(options.Args.Settings)
 	if err != nil {
 		hardexit(fmt.Sprintf("Settings could not be loaded : %s", err))
 	}
